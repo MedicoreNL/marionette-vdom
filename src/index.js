4643be14616f8cf456b76807b39227964dcca848
Arnout Evers <arnout.evers@medicore.nl>
2018-10-15 16:28:36 +0200

    master: 4.x compatibility fixes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ require('./outer-html');
 var Marionette = require('backbone.marionette');
 var VDOMMixin = require('./vdom-mixin');
 var VDOMView = Marionette.View.extend(VDOMMixin(Marionette.View.prototype));
-var VDOMCompositeView = Marionette.CompositeView.extend(VDOMMixin(Marionette.CompositeView.prototype));
 
 module.exports = {
-  View: VDOMView,
-  CompositeView: VDOMCompositeView
+  View: VDOMView
 };
